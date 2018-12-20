bfd17811e5332ab6536b39beea736899d942c200
Yaroslav Trotsai <ytrot@macmini5248.ddns.softservecom.com>
2018-12-20 17:45:42 +0200

    second test

--- spec/features/first_spec.rb
+++ spec/features/first_spec.rb
@@ -4,6 +4,9 @@ feature "Visit wikipedia page", js: true do
   scenario "Unregistered visitor visits home page" do
     visit 'https://en.wikipedia.org'
     expect(page).to have_content 'Welcome to Wikipedia'
+
+    find(:xpath, '//*[@id="mp-topbanner"]/div/div[1]/a').click
+    expect(page).to have_content 'From Wikipedia, the free encyclopedia'
   end
 
 end
